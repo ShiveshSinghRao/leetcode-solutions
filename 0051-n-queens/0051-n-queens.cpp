55582f490d942a1ac681bc1cd757eb0cd081776a
Shivesh Singh Rao <94308757+ShiveshSinghRao@users.noreply.github.com>
2023-06-27 12:59:18 +0530

    Time: 12 ms (41.09%), Space: 7.1 MB (70.91%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    bool isSafe(int row,int col,vector<string>&board,int n )
+    {
+        int dupr =row;
+        int dupc = col;
+        while(row>=0 && col>=0)
+        {
+            if(board[row][col]=='Q')return 0;
+            row--;
+            col--;
+        }
+        row = dupr;
+        col= dupc;
+        while( col>=0)
+        {
+            if(board[row][col]=='Q')return 0;
+            
+            col--;
+        }
+         row = dupr;
+        col= dupc;
+        while(row<n && col>=0)
+        {
+            if(board[row][col]=='Q')return 0;
+            row++;
+            col--;
+        }
+        return 1;
+    }
+    void callout(int col,vector<string>&board,vector<vector<string>>&ans,int n)
+    {
+        if(col==n)
+        {
+            ans.push_back(board);
+            return ;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(isSafe(i,col,board,n))
+            {
+                board[i][col]='Q';
+                callout(col+1,board,ans,n);
+                board[i][col]='.';
+            }
+        }
+
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>ans;
+        vector<string>board;
+        string str;
+        for(int i =0;i<n;i++)
+        {
+            str.push_back('.');
+        }
+        for(int i =0;i<n;i++)
+        {
+            board.push_back(str);
+        }
+        callout(0,board,ans,n);
+        return ans;
+    }
+};
