02ded52dcf2c6e3f87ff2e769b17e1140cd6933f
Shivesh Singh Rao <94308757+ShiveshSinghRao@users.noreply.github.com>
2023-08-17 21:33:00 +0530

    Time: 1587 ms (5.61%), Space: 51.3 MB (9.41%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int ans=0;
+    int check(int ind,vector<int>&nums,vector<int>& dp)
+    {
+      
+        if(ind>=nums.size()-1)
+        {
+            //ans=1;
+            return 1 ;
+        }
+        if(dp[ind]!=-1)return dp[ind];
+        //if(nums[ind]==0)return;
+        for(int i =1;i<=nums[ind];i++)
+        {
+           dp[ind]= check(i+ind,nums,dp);
+           if(dp[ind]==1)break;
+        }   
+
+        return dp[ind]==1?dp[ind]:0;
+
+    }
+    bool canJump(vector<int>& nums) {
+        vector<int>dp(nums.size(),-1);
+        return check(0,nums,dp);
+        
+    }
+};
