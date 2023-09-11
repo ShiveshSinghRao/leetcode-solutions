7c6de2a3ab36380f0c1699bb35e575146d36cd4f
Shivesh Singh Rao <94308757+ShiveshSinghRao@users.noreply.github.com>
2023-09-11 18:31:32 +0530

    Time: 4 ms (95.05%), Space: 13.2 MB (46.45%) - LeetHub

--- 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,44 +1,46 @@
 class Solution {
 public:
-//    static bool comp(pair<int,int>a,pair<int,int>b)
-//    {
-//        if(a.first<b.first)return 1;
-//        if(a.first==b.first)
-//        {
-//            if(a.second)
-//        }
-//    }
-    vector<vector<int>> groupThePeople(vector<int>&nums) {
-       vector<pair<int,int>>v;
-       vector<vector<int>>ans;
-        int n = nums.size();
+    vector<vector<int>> groupThePeople(vector<int>& nums) {
+        map<int,vector<int>>m;
+        vector<vector<int>>ans;
+        int  n =nums.size();
         for(int i =0;i<n;i++)
         {
-           v.push_back({nums[i],i});
+            m[nums[i]].push_back(i);
         }
-        sort(v.begin(),v.end());
-        for(auto x:v)
-        {
-            cout<<x.first<<" "<<x.second<<endl;
-        }
-        cout<<endl;
-        for(int i =0;i<n;i++)
-        {
-            int size = v[i].first;
-            vector<int>t;
-            cout<<" t.size() "<<t.size()<<endl;
-            for(int j =0;j<size;j++)
-            {
-               t.push_back(v[i++].second);
-               cout<<t.back()<<" ";  
-               
-            }
-            cout<<endl<<ans.size()<<endl;
-            i--;
-            cout<<endl;
 
-            ans.push_back(t);
-        }
-        return ans;
+        for(auto x:m)
+        {
+            vector<int>&t=x.second;
+            if(t.size()==x.first)
+            {
+                ans.push_back(t);
+                continue;
+            }
+           
+               vector<int>q;
+               for(auto y:t )
+               {
+                   
+                   if(q.size()<x.first)
+                     {
+                         q.push_back(y);
+                         //cout<<"q.size() "<<q.size()<<" y "<<y<<endl;
+                     }
+                    else
+                      {
+                          ans.push_back(q);
+                          // for(auto p:q)cout<<p<<" ";
+                         // cout<<endl;
+                          q.clear();
+                           q.push_back(y);
+                      }
+               }
+               if(q.size()>0)ans.push_back(q);
+            
+          
+           
+        }  return ans;
     }
+
 };
